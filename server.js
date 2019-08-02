c56011c97b1495bb30751e7c892958308c2eb2d1
Vschool computer <learn@vschool.io>
2019-08-02 14:19:47 -0600

    deploy branch

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 7000
 const morgan = require('morgan')
+const path = require("path")
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
 
 // Middlewares for every request
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 // DB connection
-mongoose.connect("mongodb://localhost:27017/todos", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/todos", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true
 })
 .then(() => console.log("connected to the DB"))
 .catch(err => console.log(err))
+
 
 //Gatekeeper.  Checks for a token on requests to /api, 
     // if there is a token, it will create the req.user object
@@ -36,6 +39,11 @@ app.use((err, req, res, next) => {
     }
     return res.send({errMsg: err.message})
 })
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Server listen
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
